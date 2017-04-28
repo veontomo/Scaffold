407fdac39c6d755ba96808a74d72f8c1e6d96cf8
Andrey Shcherbakov <veontomo@gmail.com>
2017-04-28 16:03:07 +0200

    Add the reactive-streams-1.0.0.jar to the library

--- app/src/main/Model.kt
+++ app/src/main/Model.kt
@@ -6,7 +6,7 @@ import io.reactivex.functions.Action
  */
 class Model{
     fun start(fileName: String, folderName: String, pattern: String): Completable {
-        return Completable.fromAction({print("Compleatble done")})
+        return Completable.complete()
 
 
     }
