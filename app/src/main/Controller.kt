901bc5c757463929e7e8d335cfaf5166d397a52e
Andrey Shcherbakov <veontomo@gmail.com>
2017-05-05 10:22:28 +0200

    Add custom parameters for scaffold name (range, step etc)

--- app/src/main/Controller.kt
+++ app/src/main/Controller.kt
@@ -9,16 +9,33 @@ import java.net.URL
 import java.util.*
 
 class Controller : Initializable {
+    private val EMPTY = ""
     private val model = Model()
     @FXML private var start: Button? = null
     @FXML private var clear: Button? = null
     @FXML private var selectedFiles: TextField? = null
     @FXML private var folderName: TextField? = null
     @FXML private var pattern: TextField? = null
+    @FXML private var placeholder: TextField? = null
+    @FXML private var range: TextField? = null
+    @FXML private var padding: TextField? = null
+    @FXML private var step: TextField? = null
+
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         start?.onAction = EventHandler {
-            model.start("a", "b", "c").subscribe({ print("Done") }, {print("Error")} )
+
+            model.start(fileName = selectedFiles?.text ?: EMPTY,
+                    folderName = folderName?.text ?: EMPTY,
+                    pattern = pattern?.text ?: EMPTY,
+                    placeholder = placeholder?.text ?: EMPTY,
+                    range = range?.text ?: EMPTY,
+                    padding = padding?.text ?: EMPTY,
+                    step = step?.text ?: EMPTY)
+
+                    .subscribe(
+                            { print("Done") },
+                            { e -> print(e.message) })
         }
         clear?.onAction = EventHandler { clearInputField() }
 
--- app/src/main/Model.kt
+++ app/src/main/Model.kt
@@ -1,13 +1,23 @@
 import io.reactivex.Completable
 import io.reactivex.functions.Action
+import java.io.File
 
 /**
  *  Model for the main screen.
  */
-class Model{
-    fun start(fileName: String, folderName: String, pattern: String): Completable {
-        return Completable.complete()
+class Model {
+    fun start(fileName: String, folderName: String, pattern: String, placeholder: String, range: String, step: String, padding: String): Completable {
+        val file = File(fileName)
+        if (!file.exists()) {
+            return Completable.error(Throwable("File $fileName not found."))
+        }
 
+        val folder = File(folderName)
+        if (!folder.exists()) {
+            return Completable.error(Throwable("Folder $folderName not found."))
+        }
+
+        return Completable.complete()
 
     }
 }
