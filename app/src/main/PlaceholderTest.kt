5f7820be9c4032b77920e0bba32e69aaecddb5c4
Andrey Shcherbakov <veontomo@gmail.com>
2017-05-05 11:53:18 +0200

    Add JUnit5 to the project

---
+++ app/src/main/PlaceholderTest.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * Created by Andrey on 05/05/2017.
+ */
+internal class PlaceholderTest {
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun expand() {
+    }
+
+}
