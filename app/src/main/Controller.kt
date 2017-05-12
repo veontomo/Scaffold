93f763c400bc28e740f1bbe110f0af9439682dfc
Andrey Shcherbakov <veontomo@gmail.com>
2017-05-12 17:02:27 +0200

    Save the latest inserted values into the text fields

--- app/src/main/Controller.kt
+++ app/src/main/Controller.kt
@@ -6,69 +6,78 @@ import javafx.scene.control.TextField
 import javafx.scene.text.Text
 import javafx.stage.DirectoryChooser
 import javafx.stage.FileChooser
-import java.io.File
 import java.net.URL
 import java.util.*
+import java.util.prefs.Preferences
 
 class Controller : Initializable {
     private val EMPTY = ""
     private val model = Model()
-    @FXML private var start: Button? = null
-    @FXML private var clear: Button? = null
+    @FXML private var startBtn: Button? = null
+    @FXML private var clearBtn: Button? = null
     @FXML private var selectedFiles: TextField? = null
-    @FXML private var folderName: TextField? = null
-    @FXML private var pattern: TextField? = null
+    @FXML private var folderNameField: TextField? = null
+    @FXML private var patternField: TextField? = null
     @FXML private var placeholderField: TextField? = null
     @FXML private var startField: TextField? = null
     @FXML private var endField: TextField? = null
     @FXML private var paddingField: TextField? = null
     @FXML private var stepField: TextField? = null
     @FXML private var selectFileBtn: Button? = null
-    @FXML private var selectFolder: Button? = null
+    @FXML private var selectFolderBtn: Button? = null
     @FXML private var message: Text? = null
 
 
     private val fileChooser = FileChooser()
-    private val direcoryChooser = DirectoryChooser()
+    private val directoryChooser = DirectoryChooser()
 
+    val indexedFields = setOf(patternField, paddingField, startField, endField, paddingField)
     /**
      * String used to separate different file names
      */
     private val separator = ","
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
-        start?.onAction = EventHandler {
+        restoreLatestData(indexedFields)
+        startBtn?.onAction = EventHandler {
+            val names = selectedFiles?.text?.split(separator)?.map { it.trim() }?.toTypedArray() ?: arrayOf<String>()
+            val folder = folderNameField?.text ?: EMPTY
+            val pattern = patternField?.text ?: EMPTY
+            val marker = placeholderField?.text ?: EMPTY
+            val start = startField?.text ?: EMPTY
+            val end = endField?.text ?: EMPTY
+            val padding = paddingField?.text ?: EMPTY
+            val step = stepField?.text ?: EMPTY
 
-            val names = selectedFiles?.text?.split(separator)?.map { it.trim() }?.toTypedArray()
-
-            model.start(fileNames = names ?: arrayOf<String>(),
-                    folderName = folderName?.text ?: EMPTY,
-                    pattern = pattern?.text ?: EMPTY,
-                    marker = placeholderField?.text ?: EMPTY,
-                    start = startField?.text ?: EMPTY,
-                    end = endField?.text ?: EMPTY,
-                    padding = paddingField?.text ?: EMPTY,
-                    step = stepField?.text ?: EMPTY)
+            saveLatestData(indexedFields)
+            model.start(fileNames = names,
+                    folderName = folder,
+                    pattern = pattern,
+                    marker = marker,
+                    start = start,
+                    end = end,
+                    padding = padding,
+                    step = step)
 
                     .subscribe(
                             { setMessage("Done") },
                             { e -> setMessage(e.message ?: "An error occurred") })
         }
-        clear?.onAction = EventHandler { clearInputFields(setOf(selectedFiles, pattern, folderName)) }
+        clearBtn?.onAction = EventHandler { clearInputFields(setOf(selectedFiles, patternField, folderNameField)) }
         selectFileBtn?.setOnAction { onSelectFiles() }
-        selectFolder?.setOnAction { onFolderSelected() }
+        selectFolderBtn?.setOnAction { onFolderSelected() }
 
     }
 
     private fun onFolderSelected() {
-        val folder = direcoryChooser.showDialog(selectFolder!!.scene.window)
+        val folder = directoryChooser.showDialog(selectFolderBtn!!.scene.window)
         folder?.let {
             setFolder(it.absolutePath)
         }
     }
 
-    private fun setFolder(path: String){
-        folderName?.text = path
+    private fun setFolder(path: String) {
+        folderNameField?.text = path
     }
 
     /**
@@ -94,4 +103,24 @@ class Controller : Initializable {
     fun setMessage(txt: String) {
         message?.text = txt
     }
+
+
+    fun saveLatestData(fields: Set<TextField?>) {
+        val prefs = Preferences.userNodeForPackage(this::class.java)
+        fields.forEach {
+            field ->
+            field?.let { prefs.put(it.id, it.text) }
+        }
+        prefs.flush()
+    }
+
+
+    fun restoreLatestData(fields: Set<TextField?>) {
+        val prefs = Preferences.userNodeForPackage(this::class.java)
+        fields.forEach { field ->
+            field?.let { it.text = prefs.get(field.id, null) }
+        }
+    }
+
 }
+
