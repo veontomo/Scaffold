22726bb44561458787500b7747b59408f28775c0
Andrey Shcherbakov <veontomo@gmail.com>
2017-05-15 15:44:09 +0200

    Set up the app logo

--- app/src/main/Main.kt
+++ app/src/main/Main.kt
@@ -1,6 +1,7 @@
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.image.Image
 import javafx.scene.layout.GridPane
 import javafx.stage.Stage
 
@@ -16,10 +17,16 @@ class Main : Application() {
      */
     private val LAYOUT = "mainview.fxml"
 
+    /**
+     * Path to the app logo
+     */
+    private val ICON_PATH ="/logo.png"
+
     override fun start(primaryStage: Stage) {
         primaryStage.title = "Scaffold"
 
         val grid = FXMLLoader.load<GridPane>(javaClass.getResource(LAYOUT))
+        primaryStage.icons.add(Image(ICON_PATH))
         primaryStage.scene = Scene(grid, primaryStage.width - grid.padding.left - grid.padding.right, 500.0)
         primaryStage.show()
     }
